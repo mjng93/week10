3337978d202ec827323b4603cb6090823ee79838
Michael Ng <michael.joseph.ng@gmail.com>
2020-03-12 00:02:16 +0000

    edit rsvp

--- app.rb
+++ app.rb
@@ -73,11 +73,18 @@ get "/rsvps/:id/edit" do
 
     @rsvp = rsvps_table.where(id: params["id"]).to_a[0]
     @event = events_table.where(id: @rsvp[:event_id]).to_a[0]
+    
     view "edit_rsvp"
 end
 
 post "/rsvps/:id/update" do
     puts "params: #{params}"
+  
+    @rsvp = rsvps_table.where(id: params["id"]).to_a[0] #remember, this route is stateless, so we only know the id of the rsvp
+    @event = events_table.where(id: @rsvp[:event_id]).to_a[0]
+    rsvps_table.where(id: params["id"]).update(
+        comments: params["comments"],
+        going: params["going"])
 
     view "update_rsvp"
 end
